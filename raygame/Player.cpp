5865a4d566a97d37892c6a4f68517d39ff9299d4
CamRobinson <robinsoncameron1103@gmail.com>
2021-01-13 12:06:04 -0600

    Added Player Class

---
+++ raygame/Player.cpp
@@ -0,0 +1,50 @@
+#include "Player.h"
+#include "Actor.h"
+
+void Player::player(std::string name, std::string area)
+{
+    setName(name);
+    setArea(area);
+    setMaxHealth();
+    setHealth(playerMaxHealth);
+    setDamage();
+}
+
+void Player::setName(std::string name)
+{
+    playerName = name;
+}
+
+void Player::setArea(std::string area)
+{
+    playerArea = area;
+}
+
+void Player::setHealth(double health)
+{
+    playerHealth = health;
+}
+
+
+std::string Player::getName()
+{
+    return playerName;
+}
+
+std::string Player::getArea()
+{
+    return playerArea;
+}
+
+double Player::getHealth()
+{
+    return playerHealth;
+}
+double Player::getMaxHealth()
+{
+    return playerMaxHealth;
+}
+int Player::getDamage()
+{
+    return playerDamage;
+}
